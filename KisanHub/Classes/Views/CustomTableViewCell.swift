8915a98310acacba54903544f6c98f8205aab110
Vrushali Kulkarni <kulkarni.vrushali281@gmail.com>
2018-12-02 11:22:44 +0530

    Project Setup

---
+++ KisanHub/Classes/Views/CustomTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CustomTableViewCell.swift
+//  KisanHub
+//
+//  Created by Vrushali Kulkarni on 02/12/18.
+//  Copyright © 2018 Vrushali Kulkarni. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
