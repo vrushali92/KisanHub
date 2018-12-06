5e90d8beb4701b96e9943c5758f17e3628d17538
Vrushali Kulkarni <kulkarni.vrushali281@gmail.com>
2018-12-06 21:59:43 +0530

    Code cleanup and replaced custom dropdown with datpickerview

--- KisanHub/Classes/ViewControllers/WeatherReportViewController.swift
+++ KisanHub/Classes/ViewControllers/WeatherReportViewController.swift
@@ -11,20 +11,12 @@ import Charts
 
 final class WeatherReportViewController: UIViewController {
     
-    private static let nibname = "CustomTableViewCell"
-    private static let cellIdentifier = "CustomTableViewCellIdentifier"
-    
     @IBOutlet private weak var locationSegmentedControl: UISegmentedControl!
-    @IBOutlet private weak var yearTableViewHeightConstraint: NSLayoutConstraint!
-    @IBOutlet private weak var yearTableView: UITableView!
+    @IBOutlet weak var yearTextField: UITextField!
     @IBOutlet private weak var graphView: LineChartView!
 
     private var yearArray = [2017, 2001, 2000, 1997, 1910]
-    
-    private var previousRowSelected: Int?
-    private var isDropdownOpen: Bool = false
-    private var selectedYearIndex: Int = 0
-    
+
     private lazy var weatherReportModel: WeatherReportViewModel = {
         let viewModel = WeatherReportViewModel()
         viewModel.eventDelegate = self
@@ -35,15 +27,13 @@ final class WeatherReportViewController: UIViewController {
         super.viewDidLoad()
         self.configureUI()
         self.fetchReport(forLocation: .unitedKingdom)
-        performOnMain {
-            self.updateYears()
-        }
+        self.updateYears()
     }
     
     private func configureUI() {
         self.configureSegmentControl()
-        self.configureTableView()
         self.configureChart()
+        self.configureDatePickerView()
     }
     
     private func configureSegmentControl() {
@@ -56,23 +46,24 @@ final class WeatherReportViewController: UIViewController {
         self.locationSegmentedControl.selectedSegmentIndex = 0
     }
     
-    private func configureTableView() {
+    private func configureDatePickerView() {
         
-        self.yearTableView.estimatedRowHeight = 36.0
-        self.yearTableView.rowHeight = UITableView.automaticDimension
+        let datePickerView = UIDatePicker()
+        datePickerView.datePickerMode = .date
+        datePickerView.addTarget(self,
+                                 action: #selector(WeatherReportViewController.dateChanged(datePickerView:)),
+                                 for: .valueChanged)
         
-        self.yearTableView.estimatedSectionHeaderHeight = 36.0
-        self.yearTableView.sectionHeaderHeight = UITableView.automaticDimension
-        
-        let typeSelf = type(of: self)
-        let nib = UINib(nibName: typeSelf.nibname, bundle: nil)
-        self.yearTableView.register(nib, forCellReuseIdentifier: typeSelf.cellIdentifier)
-        
-        let headerNib = UINib(nibName: String(describing: DropdownHeaderView.classForCoder()), bundle: nil)
-        self.yearTableView.register(headerNib, forHeaderFooterViewReuseIdentifier: DropdownHeaderView.identifier)
-        self.yearTableView.dataSource = self
-        self.yearTableView.delegate = self
+        self.yearTextField.inputView = datePickerView
+    }
     
+    @objc func dateChanged(datePickerView: UIDatePicker) {
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy"
+        self.yearTextField.text = dateFormatter.string(from: datePickerView.date)
+        self.weatherReportModel.eventDelegate?.handle(event: .reportAvailable)
+        self.view.endEditing(true)
     }
     
     private func configureChart() {
@@ -95,87 +86,8 @@ final class WeatherReportViewController: UIViewController {
         guard let selectedLocation = self.locationSegmentedControl.titleForSegment(at: self.locationSegmentedControl.selectedSegmentIndex) else { return }
         
         self.fetchReport(forLocation: Location.location(fromString: selectedLocation))
+        self.weatherReportModel.eventDelegate?.handle(event: .reportAvailable)
         self.updateYears()
-    }
-}
-
-extension WeatherReportViewController {
-    
-    private func handleDropDown() {
-        
-        let rowHeight: CGFloat
-        
-        if self.isDropdownOpen {
-            rowHeight = self.yearTableViewHeightConstraint.constant + (self.yearTableView.estimatedRowHeight * CGFloat(yearArray.count))
-        } else {
-            rowHeight = self.yearTableViewHeightConstraint.constant - (self.yearTableView.estimatedRowHeight * CGFloat(yearArray.count))
-        }
-        
-        UIView.animate(withDuration: 6) {
-            self.yearTableViewHeightConstraint.constant = rowHeight
-            self.view.setNeedsLayout()
-            self.view.updateConstraintsIfNeeded()
-        }
-    }
-}
-
-extension WeatherReportViewController: DropdownHeaderViewDelegate {
-    
-    func didSelect(_ header: DropdownHeaderView?) {
-        
-        self.isDropdownOpen = !self.isDropdownOpen
-        self.handleDropDown()
-        
-    }
-}
-
-extension WeatherReportViewController: UITableViewDataSource {
- 
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.yearArray.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: type(of: self).cellIdentifier, for: indexPath)
-        cell.textLabel?.text = String(self.yearArray[indexPath.row])
-        cell.textLabel?.textAlignment = .center
-        
-        if let previousRow = self.previousRowSelected,
-            previousRow == indexPath.row {
-            cell.accessoryType = .checkmark
-            
-        } else {
-            cell.accessoryType = .none
-        }
-        cell.backgroundColor = UIColor.yellow
-        return cell
-    }
-    
-}
-
-extension WeatherReportViewController: UITableViewDelegate {
-    
-    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        
-        let headerView = tableView.dequeueReusableHeaderFooterView(withIdentifier: DropdownHeaderView.identifier)
-        if let headerView = headerView as? DropdownHeaderView {
-            headerView.delegate = self
-            let index = self.previousRowSelected ?? 0
-            headerView.headerTitle = String(self.yearArray[index])
-        }
-        return headerView
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        tableView.deselectRow(at: indexPath, animated: true)
-        self.selectedYearIndex = indexPath.row
-        self.isDropdownOpen = false
-        self.handleDropDown()
-        self.previousRowSelected = indexPath.row
-        print(self.yearArray[indexPath.row])
-        tableView.reloadData()
-        
-        self.graphView.data = self.weatherReportModel.chartData(forYear: self.yearArray[indexPath.row])
     }
 }
 
@@ -187,7 +99,9 @@ extension WeatherReportViewController: WeatherReportViewModelEventsDelegate {
         case .failed(let error):
             print(error)
         case .reportAvailable:
-            self.graphView.data = self.weatherReportModel.chartData(forYear: self.yearArray[self.selectedYearIndex])
+            if let year = self.yearTextField.text, let year1 = Int(year) {
+                self.graphView.data = self.weatherReportModel.chartData(forYear: year1)
+            }
         }
     }
 }
